17826b35195a5020f4a3b7f38ee93abb48820497
Facundo Villalba <facu.villa@hotmail.com>
2018-06-18 17:36:02 -0300

    Imagebackground and basic style added

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View,FlatList } from 'react-native';
+import { StyleSheet, Text, ImageBackground, View,FlatList, TouchableOpacity } from 'react-native';
 
 const baseURL = 'https://api.flickr.com/services/rest/?format=json&nojsoncallback=1';
 const APIKey = '5da3b1261f4b9ba6e92143b48e065a3c';
@@ -20,21 +20,33 @@ export default class App extends React.Component {
     return options = { headers: { "Content-Type": "application/json" }}
   }
   getSets(userId){
-  	let method = 'flickr.photosets.getList';
-    let url = `${requestURL}&user_id=${userId}&method=${method}`;
+    let method = 'flickr.photosets.getList';
+    let primary_photo_extras = 'url_m';
+    let url = `${requestURL}&user_id=${userId}&method=${method}&primary_photo_extras=${primary_photo_extras}`;
+    console.log(url);
     return fetch(url).then((res) => res.json()).then((jsonRes) => this.handleResponse(jsonRes));
   }
   handleResponse(res) {
     console.log(Object.keys(res.photosets.photoset));
-    console.log(res.photosets.photoset);
+    console.log(res.photosets.photoset[0].id);
+    console.log(res.photosets.photoset[0].primary_photo_extras.url_m);
     this.setState({ userSets: res.photosets.photoset })
-    }
+  }
+
+  renderSeparator = () => {
+    return (
+      <View style={styles.separator} />
+    );
+  };
+//
   render() {
     return (
       <View style={styles.container}>
         <Text>Dane Pedersen</Text>
         <Text>Seguidores Siguiendo</Text>
-        <FlatList
+        <FlatList  
+            style={styles.list}           
+            ItemSeparatorComponent={this.renderSeparator}
             data={this.state.userSets}
             keyExtractor={item => item.id}
             renderItem={({item}) => (
@@ -43,6 +55,7 @@ export default class App extends React.Component {
                   description = {item.description._content}
                   photos = {item.photos}
                   count_views = {item.count_views}
+                  primary_photo_url = {item.primary_photo_extras.url_m}
                 />
               )}
         />
@@ -51,24 +64,70 @@ export default class App extends React.Component {
   }
 }
 //<Image source={albumPic} style={{width: 193, height: 110}}/>
+//<Image source={this.props.primary_photo_url} style={{width: 193, height: 110}}/>
 //<Text>{props.description}</Text>
-function FlickrSet(props) {
-    return (
-      <View style={styles.container}>
-        <Text>{props.title}</Text>  
-        <Text>{props.photos} photos {props.count_views} views</Text>            
-      </View>
-    );
-}
+class FlickrSet extends React.Component{
 
+  _onPress = () => {
+  };
+
+  render() {
+    return (
+      <TouchableOpacity onPress={this._onPress}>
+        <View style={styles.item}>
+          <ImageBackground 
+              source={{uri:this.props.primary_photo_url}}
+              resizeMode='cover'
+              style={styles.imgBackground}
+          >
+              <Text>{this.props.title} {"\n"}
+              {"\n"}
+              {"\n"}
+              {"\n"}
+              {"\n"}
+              {"\n"}
+              {"\n"}
+              {"\n"}
+              {this.props.photos} photos {this.props.count_views} views
+              </Text>
+          </ImageBackground>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
 const styles = StyleSheet.create({
+
   container: {
-    marginTop: 15,
+    marginTop: "8%",    
+    marginLeft: "5%",
+    marginRight: "5%",
+    marginTop: "5%",  
     flex: 1,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: 'flex-start',
     backgroundColor: "#F5FCFF"
-  }
+  },
+  list : {
+    alignSelf: 'stretch',
+    height: "80%",
+  },
+  item: { 
+    alignSelf: 'stretch',       
+  },
+  separator: {
+    height: 1,    
+    width: "98%",
+    alignSelf: "center",
+    //alignItems: 'stretch',
+    backgroundColor: "#CED0CE",
+  }, 
+  imgBackground: {
+    width: '100%',
+    height: '100%',
+    flex: 1 
+},
+
 });
 
 
